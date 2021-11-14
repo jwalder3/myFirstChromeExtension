4945781b2c1fc47fb55f293bde4872b50cadd916
Joseph Walder <joseph.walder@xceptor.com>
2021-11-14 13:08:45 +0000

    Updated Chrome Extension

--- Bear/popup.js
+++ Bear/popup.js
@@ -1,23 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
 
-  const bg = chrome.extension.getBackgroundPage()
-  Object.keys(bg.bears).forEach(function (url) {
-    const div = document.createElement('div')
-    div.textContent = `${url}: ${bg.bears[url]}`
-    document.body.appendChild(div)
-  })
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
 
-  // document.querySelector('button').addEventListener('click', onclick, false)
-  //
-  // function onclick () {
-  //   chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-  //     chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
-  //   })
-  // }
-  //
-  // function setCount (res) {
-  //   const div = document.createElement('div')
-  //   div.textContent = `${res.count} bears`
-  //   document.body.appendChild(div)
-  // }
-}, false)
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
